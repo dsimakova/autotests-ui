47ca772a93d87fdbc33dc40ba906b8108b8456c0
Dasha <dasha@uranus.local>
2025-09-23 11:37:14 +0300

    7.1 Знакомство с PageObject. Практика работы с Page Object

---
+++ pages/registration_page.py
@@ -0,0 +1,40 @@
+from playwright.sync_api import Page, expect
+
+from pages.base_page import BasePage
+
+
+class RegistrationPage(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        self.registration_button = page.get_by_test_id('registration-page-registration-button')
+
+    def fill_registration_form(self, email: str,username: str, password: str):
+        expect(self.registration_button).to_be_disabled()
+
+        self.email_input.fill(email)
+        expect(self.email_input).to_have_value(email)
+
+        self.username_input.fill(username)
+        expect(self.username_input).to_have_value(username)
+
+        self.password_input.fill(password)
+        expect(self.password_input).to_have_value(password)
+
+    def click_registration_button(self):
+        self.registration_button.click()
+    '''
+    def save_storage_state(self, path: str):
+        self.page.context.storage_state(path=path)
+    '''
+    def new_context_with_storage(self, path: str):
+        browser = self.page.context.browser
+        self.context = browser.new_context(storage_state=path)
+        self.page = self.context.new_page()
+
+
+
+
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,38 +1,20 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
+
+email = 'user.name@gmail.com'
+password = 'password'
+username = 'username'
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()  # Создание контекста
-        page = context.new_page()  # Создание страницы
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.fill_registration_form(email=email, username= username, password=password)
+    registration_page.click_registration_button()
 
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        expect(registration_button).to_be_disabled()
+    dashboard_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    dashboard_page.dashboard_title_check()
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
-
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path="browser-state.json")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
-
-        page.wait_for_timeout(5000)
