64fe14c952c38ee1296a9d59756faf4f85edacbd
Dasha <dasha@uranus.local>
2025-09-28 12:45:43 +0300

    7.3 Знакомство с PageComponent. Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -44,11 +44,10 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
 @pytest.mark.courses
 @pytest.mark.regression
 def test_empty_courses_list(courses_list_page: CoursesListPage):
-    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-    courses_list_page.navbar.check_visible("username")
+    courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
     courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible('username')
 
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_empty_view()
