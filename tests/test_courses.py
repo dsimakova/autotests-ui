c687d6cd3d033f9c10f5bc710ae06d24b5b18a96
Dasha <dasha@uranus.local>
2025-09-30 13:28:55 +0300

    7.4 Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -32,13 +32,14 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
                                                min_score = '10')
     create_course_page.click_create_course_button()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(index=0,
-                                                title = 'Playwright',
-                                                estimated_time = '2 weeks',
-                                                max_score = '100',
-                                                min_score = '10')
+    courses_list_page.toolbar_view.check_visible()
+
+    courses_list_page.course_view.check_visible(index=0,
+                                                title="Playwright",
+                                                max_score="100",
+                                                min_score="10",
+                                                estimated_time="2 weeks"
+    )
 
 
 @pytest.mark.courses
@@ -49,11 +50,8 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.sidebar.check_visible()
     courses_list_page.navbar.check_visible('username')
 
-    courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_empty_view()
-    courses_list_page.check_visible_create_course_button()
-
-
+    courses_list_page.toolbar_view.check_visible()
 
 
 
